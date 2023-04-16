fc11cf1c427890f488130d8439b7353574126546
Timo Waldschmidt <timo.waldschmidt@gmx.de>
2023-04-16 17:11:33 +0200

    Feature 4711

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world branch!!!");
-        System.out.println("Feature 4711");
-        System.out.println("Master-Branch add");
+        System.out.println("Hello world Master");
+
     }
 
 }
