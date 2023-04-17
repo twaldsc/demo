662b8e9bacde0f249fb1ef5877f5173f5710916c
Timo <32995141+twaldsc@users.noreply.github.com>
2023-04-17 18:22:23 +0200

    Update Main.java
    Weitere Funktionalität

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
 public class Main {
+    public void someWork() {
+        System.out.println("Here is some Work");
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello world Master");
-
+        Main main = new Main();
+        main.someWork();
     }
 
 }
